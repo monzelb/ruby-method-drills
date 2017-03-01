970b65f6b6156c4b18fe72ae8732645b555be855
monzelb <monzelb@gmail.com>
2017-02-28 16:39:55 -0800

    completed first three exercises

--- starter-code/arguments.rb
+++ starter-code/arguments.rb
@@ -1,18 +1,22 @@
 #########################
 #### USING ARGUMENTS ####
 #########################
-#say_hello
-  # returns 'hello'
+def say_hello
+  "hello"
+end
 
-#echo
-  # returns the input string
+def echo(str)
+  str.to_s
+end
 
-#eddie_izzards_height
-  # calculates and returns Eddie Izzard's height
-  # takes in the height of heels he's wearing (default value: 0)
-  # and adds heel height to his base 67 inches for the result
+ def eddie_izzards_height(height=0)
+  eddie_height= height + 67
+end
 
-#how_many_args
+
+def how_many_args(*args)
+  args.length
+end
   # accepts any number of arguments without complaint
   # returns the total number of arguments passed to the function
   # Hint: google ruby *args
